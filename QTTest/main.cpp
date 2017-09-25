3225fe12b701e38cd7457386fa0ccc033f257b7c
huang <1966423770@qq.com>
2017-09-25 14:13:32 +0800

    FIle_learn

--- QTTest/main.cpp
+++ QTTest/main.cpp
@@ -7,13 +7,23 @@
 #include <QTime>
 #include <QPainter>    // 系统绘制函数 
 #include <QWidget>
+#include <QEvent>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QFont>
+#include <QPainter>
+#include <QRadialGradient> 
+#include <QBrush >
+
 #include "qttest.h"    // 主窗口的创建 
 #include "Newspaper.h"
 #include "Reader.h"
 #include "CustomButton.h"
 #include "label.h"      // 事件响应与过滤的总结
 
-#define  SystemPainter 
+#define  GraphicSeceneAndView 
+#define  File 
+//#define TotalEvent 2 
   
 int main(int argc, char *argv[])
 {
@@ -116,14 +126,20 @@ int main(int argc, char *argv[])
 
 #endif   
 
-
 #ifdef TotalEvent    // 事件总结  QApplivation event filterEvent PressButton 
 	Label label;
 	app.installEventFilter(new EventFilter(&label, &label));
 	label.show();
 
 #elif 2==TotalEvent   // 自定定义事件  
-	 
+	 // QEvent::Type  ;
+	 // QEvent::User 
+	// static int QEvent::registerEventType ( int hint = -1 );   //事件注册  
+	//static bool QCoreApplication::sendEvent(QObject *receiver,QEvent *event);
+	//QMouseEvent event(QEvent::MouseButtonPress, pos, 0, 0, 0);
+	//QApplication::sendEvent(mainWindow, &event); 
+	//static void QCoreApplication::postEvent(QObject *receiver,QEvent *event); 
+
 #endif
 
 #ifdef SystemPainter
@@ -132,6 +148,110 @@ int main(int argc, char *argv[])
 	painter.show();
 
 #endif
+#ifdef GraphicSeceneAndView 
+    
+	QGraphicsScene scene;
+	scene.setSceneRect(0, 0, 500, 500);   //sceneRect属性供QGraphicsView确定视图默认的滚动条区域，并且协助QGraphicsScene管理元素索引
+	scene.addLine(0, 0, 150, 150);
+	QString text = "maybe I have get the key of QT5";
+	scene.addText(text);
+
+	QGraphicsView view(&scene);
+	view.setWindowTitle("Graphics View");      // 窗口命名
+	//view.resize(500, 500);   // 居中显示  
+	view.show();
+
+  
+#endif 
+#ifdef DrawBushAndQpen 
+
+	QRadialGradient gradient(50, 50, 50, 50, 50);
+	gradient.setColorAt(0, QColor::fromRgbF(0, 1, 0, 1));
+	gradient.setColorAt(1, QColor::fromRgbF(0, 0, 0, 0));
+	QBrush brush(gradient);
+
+	QPainter painter;
+	QPen pen(Qt::green, 3, Qt::DashDotLine, Qt::RoundCap, Qt::RoundJoin);
+	painter.setPen(pen);
+
+	painter.setBrush(brush);
+	painter.drawEllipse(50,150,200,150);
+
+	painter.setRenderHint(QPainter::Antialiasing, true);    // 反走样 
+	painter.setPen(QPen(Qt::black, 5, Qt::DashDotLine, Qt::RoundCap));
+	painter.setBrush(Qt::yellow);
+	painter.drawEllipse(300, 150, 200, 150);
+
+#endif
+
+#ifdef LinearGradient  
+
+	void paintEvent(QPaintEvent *)
+	{
+		QPainter painter(this);
+
+		painter.setRenderHint(QPainter::Antialiasing, true);
+		QLinearGradient linearGradient(60, 50, 200, 200);
+		linearGradient.setColorAt(0.2, Qt::white);
+		linearGradient.setColorAt(0.6, Qt::green);
+		linearGradient.setColorAt(1.0, Qt::black);
+		painter.setBrush(QBrush(linearGradient));
+		painter.drawEllipse(50, 50, 200, 150);
+	} 
+
+
+	void ColorWheel::paintEvent(QPaintEvent *)   // 颜色轮 
+	{
+		QPainter painter(this);
+		painter.setRenderHint(QPainter::Antialiasing);
+
+		const int r = 150;
+		QConicalGradient conicalGradient(0, 0, 0);
+
+		conicalGradient.setColorAt(0.0, Qt::red);      // 0 度角设置为红色
+		conicalGradient.setColorAt(60.0 / 360.0, Qt::yellow);
+		conicalGradient.setColorAt(120.0 / 360.0, Qt::green);
+		conicalGradient.setColorAt(180.0 / 360.0, Qt::cyan);
+		conicalGradient.setColorAt(240.0 / 360.0, Qt::blue);
+		conicalGradient.setColorAt(300.0 / 360.0, Qt::magenta);
+		conicalGradient.setColorAt(1.0, Qt::red);
+
+		painter.translate(r, r);
+
+		QBrush brush(conicalGradient);
+		painter.setPen(Qt::NoPen);
+		painter.setBrush(brush);
+		painter.drawEllipse(QPoint(0, 0), r, r);
+	}
+
+	void ColorWheel::paintEvent(QPaintEvent *)   // 颜色轮 
+	{
+		QPainter painter(this);
+		painter.setRenderHint(QPainter::Antialiasing);
+
+		const int r = 150;
+		QConicalGradient conicalGradient(r, r, 0);
+
+		conicalGradient.setColorAt(0.0, Qt::red);
+		conicalGradient.setColorAt(60.0 / 360.0, Qt::yellow);
+		conicalGradient.setColorAt(120.0 / 360.0, Qt::green);
+		conicalGradient.setColorAt(180.0 / 360.0, Qt::cyan);
+		conicalGradient.setColorAt(240.0 / 360.0, Qt::blue);
+		conicalGradient.setColorAt(300.0 / 360.0, Qt::magenta);
+		conicalGradient.setColorAt(1.0, Qt::red);
+
+		QBrush brush(conicalGradient);
+		painter.setPen(Qt::NoPen);
+		painter.setBrush(brush);
+		painter.drawEllipse(QPoint(r, r), r, r);
+	}
+
+#endif
+#ifdef  File
+	 
+
+#endif 
+
 
 	return app.exec();
 }
